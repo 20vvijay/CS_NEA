c388abd68bd1902faa9e1e720aa88198a5d0569e
Vijayaruban <vvijayarub@incora.com>
2026-08-22 18:09:04 +0100

    made a quiz

---
+++ src/StockRoute/StockRoute/Pages/Quiz.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StockRoute.Pages
+{
+    public class QuizModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
