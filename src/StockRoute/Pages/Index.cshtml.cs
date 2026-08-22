c47e7d418dc76e82afc986b05239c85c16b1d0f0
Vijayaruban <vvijayarub@incora.com>
2026-08-22 16:09:45 +0100

    changed gitignore file for the new razor pages

---
+++ src/StockRoute/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StockRoute.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
